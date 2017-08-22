7f9ed7c7f16d17e7b56e778aaf2e399560f89930
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-08-21 23:38:47 -0300

    ADd a simple redux store to the app

---
+++ src/app/app.actions.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Action } from 'redux';
+
+// EXEMPLO
+
+
+@Injectable()
+export class CounterActions {
+  static INCREMENT = 'INCREMENT';
+  static DECREMENT = 'DECREMENT';
+
+  increment(): Action {
+    return { type: CounterActions.INCREMENT };
+  }
+
+  decrement(): Action {
+    return { type: CounterActions.DECREMENT };
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+// REDUX
+import { NgReduxModule, NgRedux } from '@angular-redux/store';
+import { rootReducer, IAppState, INITIAL_STATE } from './store';
+
+// MAterial Modules
 import { MdToolbarModule, MdIconModule, MdInputModule, MdButtonModule } from '@angular/material';
 
+// My Modules
 import { CommonModule } from './common/common.module';
 
 import { AppComponent } from './app.component';
@@ -16,6 +22,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
+    NgReduxModule, //REDUX
     MdToolbarModule,
     MdInputModule,
     MdIconModule,
@@ -24,4 +31,11 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>) {
+    // Tell @angular-redux/store about our rootReducer and our initial state.
+    // It will use this to create a redux store for us and wire up all the
+    // events.
+    ngRedux.configureStore(rootReducer, INITIAL_STATE);
+  }
+}
